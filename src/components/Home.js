1fd1235a000fea7171d83e4063b0e9670b4557d7
bhl12345 <45795278+bhl12345@users.noreply.github.com>
2020-09-01 11:13:25 -0400

    added text and text props

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,14 +12,15 @@ export default class Home extends Component {
           <div className="flexslider js-fullheight">
             <ul className="slides">
               <li style={{backgroundImage: 'url(images/background-site.jpg)'}}>
-                <div className="overlay" />
+                <div className="row"  className="overlay" />
                 <div className="container-fluid">
-                  <div className="row">
+                  <div>
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1><br /></h1>
-                          <p><a   href=" " target="_blank" rel="noopener noreferrer"><i  /></a></p>
+                          <p1>You will get all you want in life,if you help enough other  
+                              people get what they want.  
+                              Zig Ziglar</p1>
                         </div>
                       </div>
                     </div>
